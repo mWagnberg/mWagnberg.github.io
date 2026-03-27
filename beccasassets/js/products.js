1c5982cddc1126e1347c05cd7e6e7803cdfccb77
mWagnberg <michael.wagnberg@gmail.com>
2026-03-27 22:17:53 +0100

    Add files via upload

---
+++ beccasassets/js/products.js
@@ -0,0 +1,70 @@
+const CONTACT_EMAIL = 'rebecca.wagnberg@gmail.com';
+
+const PRODUCTS = [
+    {
+        id: 1,
+        slug: 'gosedjur-bjorn',
+        name: 'Gosedjur Björn',
+        price: 35,
+        stock: 15,
+        palette: ['#d97b66', '#f7d6bf'],
+        image: 'beccasassets/images/bjorn.svg',
+        blurb: 'Mjuk nalle med varm scarf och klassisk björn-look.',
+        description: 'Ett mjukt gosedjur i form av en teddybjörn med stickad scarf och broderade detaljer. Passar fint som present till barnrum, dop eller som en nostalgisk detalj i ett lekfullt hem.'
+    },
+    {
+        id: 2,
+        slug: 'gosedjur-kanin',
+        name: 'Gosedjur Kanin',
+        price: 22,
+        stock: 30,
+        palette: ['#6da7a3', '#dff2ef'],
+        image: 'beccasassets/images/kanin.svg',
+        blurb: 'Ljus kanin med långa öron och mjuk rosett.',
+        description: 'Ett ljust och luftigt gosedjur med extra långa öron och en liten rosett i blå toner. Utformat för att ge en lugn, mjuk och ombonad känsla i barnrummet.'
+    },
+    {
+        id: 3,
+        slug: 'gosedjur-katt',
+        name: 'Gosedjur Katt',
+        price: 45,
+        stock: 10,
+        palette: ['#80543d', '#efdbc7'],
+        image: 'beccasassets/images/katt.svg',
+        blurb: 'Randig katt med rosett och varm honungsfärg.',
+        description: 'Detta kattgosedjur har ett lekfullt uttryck, randiga detaljer och en liten grön rosett. Ett charmigt mjukdjur för den som gillar katter och varma naturtoner.'
+    },
+    {
+        id: 4,
+        slug: 'gosedjur-panda',
+        name: 'Gosedjur Panda',
+        price: 28,
+        stock: 25,
+        palette: ['#f4b860', '#fff0cb'],
+        image: 'beccasassets/images/panda.svg',
+        blurb: 'Monokrom panda med rund form och lugnt uttryck.',
+        description: 'En panda i mjuk plysch med rund kropp och enkla, tydliga kontraster. Den fungerar fint i minimalistiska barnrum eller som en present med ett mer grafiskt uttryck.'
+    },
+    {
+        id: 5,
+        slug: 'gosedjur-elefant',
+        name: 'Gosedjur Elefant',
+        price: 18,
+        stock: 50,
+        palette: ['#4c6b8a', '#d9e8f5'],
+        image: 'beccasassets/images/elefant.svg',
+        blurb: 'Lugn elefant i dova grå toner med stora öron.',
+        description: 'Ett gosigt elefantmjukdjur med stora rosa öron och sydd snabel. Den neutrala färgpaletten gör att den passar fint i skandinaviska interiörmiljöer och barnrum.'
+    },
+    {
+        id: 6,
+        slug: 'gosedjur-rav',
+        name: 'Gosedjur Räv',
+        price: 38,
+        stock: 12,
+        palette: ['#3d7ea6', '#d5eef8'],
+        image: 'beccasassets/images/rav.svg',
+        blurb: 'Rostfärgad räv med vit bringa och mjuka former.',
+        description: 'En varm och lekfull räv i plysch med vita kontrastdetaljer framtill. Perfekt för barn som gillar skogsdjur eller för en butik som vill ha en mer sagolik kollektion.'
+    }
+];
